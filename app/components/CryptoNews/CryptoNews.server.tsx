906521909bf0f38fd73dfa9fb477084a8246f1ba
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-11-17 18:38:31 +0100

    wip

--- app/components/CryptoNews/CryptoNews.server.tsx
+++ app/components/CryptoNews/CryptoNews.server.tsx
@@ -2,7 +2,6 @@ import { CryptoAPI } from "@/app/api/crypto-api";
 import { ReactNode } from "react";
 import { CryptoNews as CryptoNewsCli } from "./CryptoNews.client";
 
-export const preferredRegion = "cdg1";
 export async function CryptoNews(p: { children: ReactNode }) {
   const crypto = await CryptoAPI.fetchAll();
   return <CryptoNewsCli initialData={crypto}>{p.children}</CryptoNewsCli>;
