7c11d74c17e0bbe98326d5e60660fe2d04b39986
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-11-24 14:00:19 +0100

    wip

--- app/components/Nav/Nav.tsx
+++ app/components/Nav/Nav.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import { NAV_ITEMS } from "./constant";
 import logoPng from "@/public/logo.png";
 import { NavItem } from "./NavItem";
+import Link from "next/link";
 export function Nav() {
   return (
     <div>
       {/* Logo */}
-      <Image src={logoPng} alt="Logo" className="w-40" />
-
+      <Link href={"/"}>
+        <Image src={logoPng} alt="Logo" className="w-40" />
+      </Link>
       {/* Nav list */}
       <nav className="space-y-4 mt-4">
         {NAV_ITEMS.map((navItem) => (
