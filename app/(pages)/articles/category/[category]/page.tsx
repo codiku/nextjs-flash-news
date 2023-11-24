a533bfbf86d68009043187012583a05621377d35
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-11-24 14:19:16 +0100

    wip

--- app/(pages)/articles/category/[category]/page.tsx
+++ app/(pages)/articles/category/[category]/page.tsx
@@ -2,14 +2,16 @@ import { CATEGORIES_ITEMS } from "@/app/constant";
 import Image from "next/image";
 
 export default function CategoryDetailPage(p: {
-  params: { id: keyof typeof CATEGORIES_ITEMS; date: string };
+  params: { category: keyof typeof CATEGORIES_ITEMS; date: string };
   searchParams: { date: string };
 }) {
-  const cat = CATEGORIES_ITEMS[p.params.id];
+  const cat = CATEGORIES_ITEMS[p.params.category];
   return (
     <div className="flex items-center space-x-4">
       <Image src={cat.src} alt={cat.alt} className="w-10 h-10" />
-      <h1 className="capitalize font-bold text-3xl">{p.params.id} News</h1>
+      <h1 className="capitalize font-bold text-3xl">
+        {p.params.category} News
+      </h1>
     </div>
   );
 }
