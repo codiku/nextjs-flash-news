59cc8d5eb62b94e373d8d89e0a554703f0e64697
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-11-27 20:19:21 +0100

    wip

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,7 +1,30 @@
 import { ArticleApi } from "@/app/api/article-api";
 
 export default async function IndexPage() {
-  const articles = await ArticleApi.fetchToday();
+  // const articles = await ArticleApi.fetchToday();
 
-  http: return <div>{JSON.stringify(articles)}</div>;
+  const fetch1 = await (
+    await fetch("http://api.open-notify.org/iss-now.json", {
+      next: { revalidate: 5 },
+    })
+  ).json();
+
+  const fetch2 = await (
+    await fetch("https://api.chucknorris.io/jokes/random", {
+      next: { revalidate: 10 },
+    })
+  ).json();
+
+  http: return (
+    <div>
+      {Math.random()}
+      <br />
+      <br />
+      <br /> {JSON.stringify(fetch1)}
+      <br />
+      <br />
+      <br /> {JSON.stringify(fetch2)}
+      {/* <br />- {JSON.stringify(articles)} */}
+    </div>
+  );
 }
