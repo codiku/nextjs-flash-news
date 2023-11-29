02fa4149eb7bbb726a92c838a152b40046601cf5
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-11-29 12:26:16 +0100

    wip

--- app/(pages)/articles/category/[category]/page.tsx
+++ app/(pages)/articles/category/[category]/page.tsx
@@ -1,7 +1,26 @@
+import { ArticleApi } from "@/app/api/article-api";
+import { ArticleList } from "@/app/components/ArticleList/ArticleList";
+import { CATEGORIES_ITEMS } from "@/app/constant";
 import { Article, ArticleCategory } from "@/app/types/article-type";
+import Image from "next/image";
 
 export default async function ArticlesByCategoryPage(p: {
   params: { category: ArticleCategory; articles: Article[] };
 }) {
-  return <div>Hi from {p.params.category}</div>;
+  const articles = await ArticleApi.fetchByCategory(p.params.category);
+  return (
+    <div>
+      <div className="flex items-center space-x-4 mb-16">
+        <Image
+          src={CATEGORIES_ITEMS[p.params.category].src}
+          className="h-10 w-10"
+          alt="Latest news icon"
+        />
+        <h1 className="text-4xl font-bold capitalize">
+          {p.params.category} news
+        </h1>
+      </div>
+      <ArticleList articles={articles} />
+    </div>
+  );
 }
--- app/api/article-api.ts
+++ app/api/article-api.ts
@@ -4,12 +4,23 @@ import {
   ArticleListResponse,
 } from "../types/article-type";
 
+const DEFAULT_PARAMS = "language=en&country=us";
+
 export class ArticleApi {
   static async fetchToday(): Promise<Article[]> {
     return (
       (await (
         await fetch(
-          `https://newsdata.io/api/1/news?apiKey=${process.env.API_KEY}&language=en&country=us`
+          `${process.env.API_BASE_URL}?apiKey=${process.env.API_KEY}&${DEFAULT_PARAMS}`
+        )
+      ).json()) as ArticleListResponse
+    ).results;
+  }
+  static async fetchByCategory(category: ArticleCategory): Promise<Article[]> {
+    return (
+      (await (
+        await fetch(
+          `${process.env.API_BASE_URL}?apiKey=${process.env.API_KEY}&${DEFAULT_PARAMS}&category=${category}`
         )
       ).json()) as ArticleListResponse
     ).results;
