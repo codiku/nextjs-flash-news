1ab1ce52e9f59440d0739179b4149a1102776966
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-11-29 10:01:04 +0100

    wip

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,10 +1,16 @@
 import { Nav } from "@/app/components/Nav/Nav";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const inter = Inter({ subsets: ["latin"] });
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className="flex p-8">
+      <body className={`${inter.className} flex p-8`}>
         <Nav />
         <div className="pl-8 mt-16 w-full">{children}</div>
       </body>
--- app/components/LatestNews/LatestNews.tsx
+++ app/components/LatestNews/LatestNews.tsx
@@ -1,11 +1,16 @@
 import { ArticleApi } from "@/app/api/article-api";
 import { ArticleCard } from "@/app/components/ArticleCard/ArticleCard";
 import { ArticleList } from "../ArticleList/ArticleList";
-
+import topPng from "@/public/top.png";
+import Image from "next/image";
 export async function LatestNews(p: {}) {
   const articles = await ArticleApi.fetchToday();
   return (
     <>
+      <div className="flex items-center space-x-4 mb-16">
+        <Image src={topPng} className="h-10 w-10" alt="Latest news icon" />
+        <h1 className="text-4xl font-bold capitalize">Latest news</h1>
+      </div>
       <ArticleList articles={articles} />
     </>
   );
