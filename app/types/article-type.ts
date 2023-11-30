d53e7241b4c3424e183b35933ee201df8c32714f
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-11-30 18:14:14 +0100

    wip

--- app/types/article-type.ts
+++ app/types/article-type.ts
@@ -9,13 +9,13 @@ export interface Article {
   article_id: string;
   title: string;
   link: string;
-  keywords?: string[] | null;
-  creator?: string[] | null;
+  keywords?: string[];
+  creator?: string[];
   video_url: any;
   description: string;
   content: string;
   pubDate: string;
-  image_url?: string | null;
+  image_url?: string;
   source_id: string;
   source_priority: number;
   country: string[];
