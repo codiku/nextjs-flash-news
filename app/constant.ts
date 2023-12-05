cc5446baed52c0239aca6815e0cf9553c0f96aaf
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-12-05 12:04:31 +0100

    wip

--- app/constant.ts
+++ app/constant.ts
@@ -1,3 +1,4 @@
+import { ArticleCategory } from "@/app/types/article-type";
 import businessPng from "@/public/business.png";
 import crimePng from "@/public/crime.png";
 import domesticPng from "@/public/domestic.png";
@@ -14,8 +15,9 @@ import technologyPng from "@/public/technology.png";
 import topPng from "@/public/top.png";
 import tourismPng from "@/public/tourism.png";
 import worldPng from "@/public/world.png";
+import { StaticImageData } from "next/image";
 
-export const CATEGORIES_ITEMS = {
+export const CATEGORIES_ITEMS: { [key in ArticleCategory]: CategoryItem } = {
   business: { src: businessPng, alt: "Business logo", category: "business" },
   crime: { src: crimePng, alt: "Crime logo", category: "crime" },
   domestic: { src: domesticPng, alt: "Domestic logo", category: "domestic" },
@@ -29,13 +31,11 @@ export const CATEGORIES_ITEMS = {
     alt: "Entertainment logo",
     category: "entertainment",
   },
-
   environment: {
     src: environmentPng,
     alt: "Environment logo",
     category: "environment",
   },
-
   food: { src: foodPng, alt: "Food logo", category: "food" },
   health: { src: healthPng, alt: "Health logo", category: "health" },
   other: { src: otherPng, alt: "Other logo", category: "other" },
@@ -47,8 +47,20 @@ export const CATEGORIES_ITEMS = {
     alt: "Technology logo",
     category: "technology",
   },
-
   top: { src: topPng, alt: "Top logo", category: "top" },
   tourism: { src: tourismPng, alt: "Tourism logo", category: "tourism" },
   world: { src: worldPng, alt: "World logo", category: "world" },
 };
+
+export const NAV_ITEMS = [
+  CATEGORIES_ITEMS.business,
+  CATEGORIES_ITEMS.environment,
+  CATEGORIES_ITEMS.technology,
+  CATEGORIES_ITEMS.world,
+];
+
+interface CategoryItem {
+  src: StaticImageData;
+  alt: string;
+  category: ArticleCategory;
+}
