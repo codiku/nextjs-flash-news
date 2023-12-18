aa6f66e961256cef321cedcd9a0b1b58088eceb3
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-12-18 19:17:03 +0100

    wip

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div className="flex h-screen w-screen justify-center items-center">
       <div>
         <h2 className="text-2xl font-bold">Not found</h2>
-        <p>Could not find requested resource</p>
+        <p>Could not find requested resource !</p>
         <Link href="/" className="text-blue-500 underline">
           Return home
         </Link>
