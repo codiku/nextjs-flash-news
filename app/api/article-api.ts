f63f0ee86916afb04b138054b1b574d1f436100a
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-12-18 16:14:24 +0100

    wip

--- app/api/article-api.ts
+++ app/api/article-api.ts
@@ -10,31 +10,23 @@ export class ArticleApi {
   static async fetchToday(): Promise<Article[]> {
     return (
       (await (
-        await fetch(
-          `${process.env.API_BASE_URL}?apiKey=${process.env.API_KEY}&${DEFAULT_PARAMS}`
-        )
+        await fetch(`${process.env.API_BASE_URL}`)
       ).json()) as ArticleListResponse
     ).results;
   }
   static async fetchByCategory(category: ArticleCategory): Promise<Article[]> {
     return (
       (await (
-        await fetch(
-          `${process.env.API_BASE_URL}?apiKey=${process.env.API_KEY}&${DEFAULT_PARAMS}
-          &category=${category}`
-        )
+        await fetch(`${process.env.API_BASE_URL}?category=${category}`)
       ).json()) as ArticleListResponse
     ).results;
   }
-  static async fetchByTitle(title: string): Promise<Article> {
+  static async fetchByTitle(title: string): Promise<Article[]> {
     const formatedTitle = title.replace("/:/g", "");
     return (
       (await (
-        await fetch(
-          `${process.env.API_BASE_URL}?apiKey=${process.env.API_KEY}&${DEFAULT_PARAMS}
-          &qInTitle=${formatedTitle}`
-        )
+        await fetch(`${process.env.API_BASE_URL}?title=${formatedTitle}`)
       ).json()) as ArticleListResponse
-    ).results[0];
+    ).results;
   }
 }
