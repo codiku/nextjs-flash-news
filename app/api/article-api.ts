2a1861d7c1506715bfd1bdbced956667637382fc
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-12-18 12:37:20 +0100

    wip

--- app/api/article-api.ts
+++ app/api/article-api.ts
@@ -4,9 +4,7 @@ export class ArticleApi {
   static async fetchToday(): Promise<Article[]> {
     return (
       (await (
-        await fetch(
-          "https://newsdata.io/api/1/news?apikey=pub_3280569b014cbfc0b9a13f2b4fd6883c50139&language=en&country=us"
-        )
+        await fetch("http://localhost:3090/news")
       ).json()) as ArticleListResponse
     ).results;
   }
