c148233d05a4986ed241f08683c0eabd9a5a76dc
Codiku <111140836+codiku-dev@users.noreply.github.com>
2023-12-19 12:03:52 +0100

    wip

--- app/api/article-api.ts
+++ app/api/article-api.ts
@@ -1,46 +1,30 @@
 import {
-  FAKE_ALL_ARTICLES,
-  FAKE_ARTICLE_BY_CATEGORIES,
-  FAKE_LATEST_ARTICLES,
-} from "../lib/mock";
-import { Article, ArticleCategory } from "../types/article-type";
-
-const DEFAULT_PARAMS = "language=en&country=us";
+  Article,
+  ArticleCategory,
+  ArticleListResponse,
+} from "../types/article-type";
 
 export class ArticleApi {
   static async fetchToday(): Promise<Article[]> {
-    // return (
-    //   (await (
-    //     await fetch(
-    //       `${process.env.API_BASE_URL}?apiKey=${process.env.API_KEY}&${DEFAULT_PARAMS}`
-    //     )
-    //   ).json()) as ArticleListResponse
-    // ).results;
-    return FAKE_LATEST_ARTICLES;
+    return (
+      (await (
+        await fetch(`${process.env.API_BASE_URL}`)
+      ).json()) as ArticleListResponse
+    ).results;
   }
   static async fetchByCategory(category: ArticleCategory): Promise<Article[]> {
-    // return (
-    //   (await (
-    //     await fetch(
-    //       `${process.env.API_BASE_URL}?apiKey=${process.env.API_KEY}&${DEFAULT_PARAMS}
-    //       &category=${category}`
-    //     )
-    //   ).json()) as ArticleListResponse
-    // ).results;
-    return FAKE_ARTICLE_BY_CATEGORIES[category];
+    return (
+      (await (
+        await fetch(`${process.env.API_BASE_URL}?category=${category}`)
+      ).json()) as ArticleListResponse
+    ).results;
   }
   static async fetchByTitle(title: string): Promise<Article> {
-    // const formatedTitle = title.replace("/:/g", "");
-    // return (
-    //   (await (
-    //     await fetch(
-    //       `${process.env.API_BASE_URL}?apiKey=${process.env.API_KEY}&${DEFAULT_PARAMS}
-    //       &qInTitle=${formatedTitle}`
-    //     )
-    //   ).json()) as ArticleListResponse
-    // ).results[0];
-    return FAKE_ALL_ARTICLES.find(
-      (article) => article.title === decodeURIComponent(title)
-    ) as Article;
+    const formatedTitle = title.replace("/:/g", "");
+    return (
+      (await (
+        await fetch(`${process.env.API_BASE_URL}?qInTitle=${formatedTitle}`)
+      ).json()) as ArticleListResponse
+    ).results[0];
   }
 }
